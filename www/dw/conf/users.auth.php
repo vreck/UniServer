0f06c740e87abd4c6559b3ae4dc300e6e57c3070
Laurent VRECK <laurent.vreck@laposte.net>
2015-04-15 18:37:21 +0200

    Add all files

---
+++ www/dw/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+laurent:$1$sA5bKrOv$pvVXbOvRDJlAft8uoIz4P.:laurent VRECK:laurent.vreck@etsi.org:admin,user
